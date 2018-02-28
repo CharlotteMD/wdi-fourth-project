4baacd5df2ebd768c8d63427571b5176314401b4
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-02-28 11:17:45 +0000

    added bootstrap

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import 'bootstrap-css-only';
 import './scss/style.scss';
 
 import Navbar from './components/utility/Navbar';
--- src/components/utility/Navbar.js
+++ src/components/utility/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import Auth from '../../lib/Auth';
-import AuthRoutes from '../auth/AuthRoutes';
+// import AuthRoutes from '../auth/AuthRoutes';
+
 
 const Navbar = ({ history }) => {
   function logout(e) {
@@ -12,82 +13,65 @@ const Navbar = ({ history }) => {
   }
 
   return(
-    <nav>
-      {/* need to define if the person is a user or hotel */}
 
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">GetARoom</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">GetARoom</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
+          {/* Not logged in */}
 
-            {/* Not logged in */}
+          { !Auth.isAuthenticated() &&
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Guests
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="/users/register">Register</a>
+              <a className="dropdown-item" href="/users/login">Login</a>
+            </div>
+          </li>}
 
-            { !Auth.isAuthenticated() &&
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Guests
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="/users/register">Register</a>
-                <a className="dropdown-item" href="/users/login">Login</a>
-              </div>
-            </li>}
+          { !Auth.isAuthenticated() &&
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Hotels
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="/hotels/register">Register</a>
+              <a className="dropdown-item" href="/hotels/login">Login</a>
+            </div>
+          </li>}
 
-            { !Auth.isAuthenticated() &&
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Hotels
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="/hotels/register">Register</a>
-                <a className="dropdown-item" href="/hotels/login">Login</a>
-              </div>
-            </li>}
+          { !Auth.isAuthenticated() &&
+          <li className="nav-item">
+            <a className="nav-link" href="/auctions">View Current Auctions</a>
+          </li>}
 
-            { !Auth.isAuthenticated() &&
-            <li className="nav-item">
-              <a className="nav-link" href="/auctions">View Current Auctions</a>
-            </li>}
+          {Auth.isAuthenticated() && Auth.getUserId() && <li className="nav-item">
+            <a href="/" className="standard-button" onClick={logout}>Logout</a>
+          </li>}
 
-            {Auth.isAuthenticated() && Auth.getUserId() && <li className="nav-item">
-              <a href="/" className="standard-button" onClick={logout}>Logout</a>
-            </li>}
+          {Auth.isAuthenticated() && Auth.getUserId() && <li className="nav-item">
+            <a className="nav-link" href={`/users/${Auth.getPayload().userId}`}>Your Profile</a>
+          </li>}
 
-            {Auth.isAuthenticated() && Auth.getUserId() && <li className="nav-item">
-              <a className="nav-link" href={`/users/${Auth.getPayload().userId}`}>Your Profile</a>
-            </li>}
+          {Auth.isAuthenticated() && Auth.getHotelId() &&<li className="nav-item">
+            <a href="/" className="standard-button" onClick={logout}>Logout</a>
+          </li>}
 
-            {Auth.isAuthenticated() && Auth.getHotelId() &&<li className="nav-item">
-              <a href="/" className="standard-button" onClick={logout}>Logout</a>
-            </li>}
+          {Auth.isAuthenticated() && Auth.getHotelId() &&
+          <li className="nav-item">
+            <a className="nav-link" href={`/hotels/${Auth.getPayload().hotelId}`}>Your Profile</a>
+          </li>}
 
-            {Auth.isAuthenticated() && Auth.getHotelId() &&
-            <li className="nav-item">
-              <a className="nav-link" href={`/hotels/${Auth.getPayload().hotelId}`}>Your Profile</a>
-            </li>}
-
-          </ul>
-        </div>
-      </nav>
-
-
-
-
-
-
-      {/* { !Auth.isAuthenticated() &&<Link to="/login" className="standard-button">Login</Link>}
-      {' '}
-      { !Auth.isAuthenticated() && <Link to="/register" className="standard-button">Register</Link>}
-      {' '}
-      { Auth.isAuthenticated() && <a href="#" className="standard-button" onClick={logout}>Logout</a>}
-      {' '}
-      { Auth.isAuthenticated() && <Link to={`/users/${Auth.getPayload().userId}`} className="standard-button">Your Profile</Link>} */}
-
+        </ul>
+      </div>
     </nav>
+
   );
 };
 
