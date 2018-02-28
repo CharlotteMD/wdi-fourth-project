a67529a1daafa2e0d9328b8da51ef458a75fd7e2
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-02-28 16:42:36 +0000

    style changes to login

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -2,29 +2,31 @@ import React from 'react';
 
 const LoginForm = ({ handleChange, handleSubmit, user }) => { // how can i make the same form use either hotel or user details?
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-          value={user.email}
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-          value={user.password}
-          className="form-control"
-        />
-      </div>
-      <button className="main-button">Login</button>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={user.email}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={user.password}
+            className="form-control"
+          />
+        </div>
+        <button className="main-button">Login</button>
+      </form>
+    </div>
   );
 };
 
