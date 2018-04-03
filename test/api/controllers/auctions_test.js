9f67af280ec6e291f838b825c80914d8eee57063
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-04-03 18:40:41 +0100

    working on testing

--- test/api/controllers/auctions_test.js
+++ test/api/controllers/auctions_test.js
@@ -78,27 +78,31 @@ describe('Auctions Controller Test', () => {
     let token = null;
 
     beforeEach(done => {
+      User
+        .create(testUser)
+        .then(user => {
+          userCreated = user;
+
+          // return User.create(testUser);
+        });
       Hotel
         .create(testHotel)
         .then(hotel => {
           hotelCreated = hotel;
 
-          return User.create(testUser);
+        });
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'megz@hotmail.com',
+          password: 'password'
         })
-        .then(user => {
-          userCreated = user;
+        .end((err, res) => {
 
-          api
-            .post('/api/login')
-            .set('Accept', 'application/json')
-            .send({
-              email: 'megz@hotmail.com',
-              password: 'password'
-            })
-            .end((err, res) => {
-              token = res.body.token;
-              done();
-            });
+          token = res.body.token;
+
+          done();
         });
     });
 
@@ -120,265 +124,37 @@ describe('Auctions Controller Test', () => {
           expect(res.status).to.eq(201);
           done();
         });
+
+      it('should return created auction data in response body', done => {
+        api
+          .post('/api/auctions')
+          .set('Accept', 'application/json')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            hotel: testHotel[0],
+            reservePrice: 500,
+            checkInDate: '01/03/2018',
+            nights: 1,
+            maxGuests: 3,
+            board: 'Bed only',
+            details: 'We are delighted to be able to offer our honeymoon suite. The offer will include the master bedroom and ajoining leisure rooms.'
+          })
+          .end((err, res) => {
+            expect(res.body)
+              .to.be.an('object')
+              .and.have.all.keys([
+                'hotel',
+                'reservePrice',
+                'checkInDate',
+                'nights',
+                'maxGuests',
+                'board',
+                'details'
+              ]);
+
+            done();
+          });
+      });
     });
   });
-
-
-  it('should return created auction data in response body', done => {
-    api
-      .post('/api/auctions')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
-        hotel: testHotel[0],
-        reservePrice: 500,
-        checkInDate: '01/03/2018',
-        nights: 1,
-        maxGuests: 3,
-        board: 'Bed only',
-        details: 'We are delighted to be able to offer our honeymoon suite. The offer will include the master bedroom and ajoining leisure rooms.'
-      })
-      .end((err, res) => {
-        expect(res.body)
-          .to.be.an('object')
-          .and.have.all.keys([
-            'hotel',
-            'reservePrice',
-            'checkInDate',
-            'nights',
-            'maxGuests',
-            'board',
-            'details'
-          ]);
-
-        done();
-      });
-  });
 });
-
-// // SHOW ROUTE
-// describe('GET /api/auctions/:id', () => {
-//   let hotelCreated = null;
-//   let userCreated = null;
-//   let auctionCreated = null;
-//   let token = null;
-//
-//   beforeEach(done => {
-//     Hotel
-//       .create(testHotel)
-//       .then(hotel => {
-//         hotelCreated = hotel;
-//
-//         return User.create(testUser);
-//       })
-//       .then(user => {
-//         userCreated = user;
-//
-//         testAuction.createdBy = user;
-//         testAuction.attendees = [user];
-//         testAuction.hotel = hotelCreated;
-//
-//         return Auction.create(testAuction);
-//       })
-//       .then(auction => {
-//         auctionCreated = auction;
-//
-//         api
-//           .post('/api/login')
-//           .set('Accept', 'application/json')
-//           .send({
-//             email: 'patrick@ga.co',
-//             password: 'patrick'
-//           })
-//           .end((err, res) => {
-//             token = res.body.token;
-//             done();
-//           });
-//       });
-//
-//   });
-//
-//   it('should return a 200 response', done => {
-//     api
-//       .get(`/api/auctions/${auctionCreated.id}`)
-//       .set('Accept', 'application/json')
-//       .set('Authorization', `Bearer ${token}`)
-//       .expect(200, done);
-//   });
-//
-//   it('should return auction data in response body', done => {
-//     api
-//       .get(`/api/auctions/${auctionCreated.id}`)
-//       .set('Accept', 'application/json')
-//       .set('Authorization', `Bearer ${token}`)
-//       .end((err, res) => {
-//         expect(res.body)
-//           .to.be.an('object')
-//           .and.have.all.keys([
-//             '__v',
-//             '_id',
-//             'id',
-//             'auctionName',
-//             'createdBy',
-//             'attendees',
-//             'dateArrive',
-//             'dateDepart',
-//             'hotel',
-//             'comments'
-//           ]);
-//
-//         done();
-//       });
-//   });
-// });
-//
-// // UPDATE ROUTE
-// describe('PUT /api/auctions/:id', () => {
-//   let hotelCreated = null;
-//   let userCreated = null;
-//   let auctionCreated = null;
-//   let token = null;
-//
-//   beforeEach(done => {
-//     Hotel
-//       .create(testHotel)
-//       .then(hotel => {
-//         hotelCreated = hotel;
-//
-//         return User.create(testUser);
-//       })
-//       .then(user => {
-//         userCreated = user;
-//
-//         testAuction.createdBy = user;
-//         testAuction.attendees = [user];
-//         testAuction.hotel = hotelCreated;
-//
-//         return Auction.create(testAuction);
-//       })
-//       .then(auction => {
-//         auctionCreated = auction;
-//
-//         api
-//           .post('/api/login')
-//           .set('Accept', 'application/json')
-//           .send({
-//             email: 'patrick@ga.co',
-//             password: 'patrick'
-//           })
-//           .end((err, res) => {
-//             token = res.body.token;
-//             done();
-//           });
-//       });
-//
-//   });
-//
-//   it('should return a 200 response', done => {
-//     var updatedAuction = {
-//       auctionName: 'Updated',
-//       dateArrive: '99 Feb',
-//       dateDepart: '99 Feb'
-//     };
-//
-//     api
-//       .put(`/api/auctions/${auctionCreated.id}`)
-//       .set('Accept', 'application/json')
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(updatedAuction)
-//       .expect(200, done);
-//   });
-//
-//   it('should return updated data in body', done => {
-//     var updatedAuction = {
-//       auctionName: 'Updated',
-//       dateArrive: '99 Feb',
-//       dateDepart: '99 Feb'
-//     };
-//
-//     api
-//       .put(`/api/auctions/${auctionCreated.id}`)
-//       .set('Accept', 'application/json')
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(updatedAuction)
-//       .end((err, res) => {
-//         expect(res.body)
-//           .to.be.an('object')
-//           .and.to.have.property('auctionName', 'Updated');
-//         done();
-//       });
-//   });
-// });
-//
-// // DELETE ROUTE
-// describe('DELETE /api/auctions/:id', () => {
-//   let hotelCreated = null;
-//   let userCreated = null;
-//   let auctionCreated = null;
-//   let token = null;
-//
-//   beforeEach(done => {
-//     Hotel
-//       .create(testHotel)
-//       .then(hotel => {
-//         hotelCreated = hotel;
-//
-//         return User.create(testUser);
-//       })
-//       .then(user => {
-//         userCreated = user;
-//
-//         testAuction.createdBy = user;
-//         testAuction.attendees = [user];
-//         testAuction.hotel = hotelCreated;
-//
-//         return Auction.create(testAuction);
-//       })
-//       .then(auction => {
-//         auctionCreated = auction;
-//
-//         api
-//           .post('/api/login')
-//           .set('Accept', 'application/json')
-//           .send({
-//             email: 'patrick@ga.co',
-//             password: 'patrick'
-//           })
-//           .end((err, res) => {
-//             token = res.body.token;
-//             done();
-//           });
-//       });
-//
-//   });
-//
-//   it('should return a 204 response', done => {
-//     api
-//       .delete(`/api/auctions/${auctionCreated.id}`)
-//       .set('Accept', 'application/json')
-//       .set('Authorization', `Bearer ${token}`)
-//       .expect(204, done);
-//   });
-//
-//
-//   describe('Auctions Controller Test', () => {
-//     describe('GET /api/auctions', () => {
-//       it('should return a 200 response', function(done) {
-//         api
-//           .get('/api/auctions')
-//           .set('Accept', 'application/json')
-//           .expect(200, done);
-//       });
-//
-//       it('should return an array of auctions', function(done) {
-//         api
-//           .get('/api/auctions')
-//           .set('Accept', 'application/json')
-//           .end((err, res) => {
-//             expect(res.body).to.be.an('array');
-//             done();
-//           });
-//       });
-//     });
-//   });
-// });
